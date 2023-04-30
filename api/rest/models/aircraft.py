2ba74926a12d5e3a7610554920c3ceeca64d32bf
Gal Sternad <gal.sternad@student.um.si>
2023-04-30 18:39:34 +0200

    New commit

--- api/rest/models/aircraft.py
+++ api/rest/models/aircraft.py
@@ -1,10 +1,15 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 
 class Aircraft(models.Model):
-    model = {
-        'code': models.CharField(max_length=255, blank=False),
-        'name': models.CharField(max_length=255, blank=False)
-    }
+    model = models.JSONField(validators=[lambda v: validate_model_field(v)])
     registration = models.CharField(max_length=255, blank=False)
     airline = models.ForeignKey('Airline', blank=False, on_delete=models.CASCADE)
+
+
+# funkcija validira vnos ključev v 'model' polje modela letala
+def validate_model_field(model):
+    allowed_keys = ['code', 'name']
+    for key in model.keys():
+        if key not in allowed_keys:
+            raise ValidationError('Invalid key \'{key}\' in aircraft model')
