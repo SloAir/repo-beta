c904f505bedc8523bd057607684f44c179bd5506
Lovro Dermota <lovro.dermota@gmail.com>
2023-05-02 13:01:52 +0200

    Dodajanje scraper_random za Principe - Scrape podatkov iz :https://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/observationAms_si_latest.html - Prikaz teh podatkov v konzoli.

---
+++ scraper_random/src/jvmMain/kotlin/data/model/WeatherData.kt
@@ -0,0 +1,9 @@
+package data.model
+
+data class WeatherData(
+    val location: String,
+    val temperature: String,
+    val humidity: String,
+    val rainfall: String,
+    val snowfall: String
+)
