420d466a946f9ac0b27448362d1363f401efc141
Gal Sternad <gal.sternad@student.um.si>
2023-06-04 18:21:12 +0200

    SLOAIR-26

--- api/rest/settings.py
+++ api/rest/settings.py
@@ -19,6 +19,7 @@ from mongoengine import get_db
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
